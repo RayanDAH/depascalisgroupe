64547e7ba37e9d6a7619fc242b475dfcd3036619
Maxime <maxime.cottin25@gmail.com>
2025-08-20 12:38:46 +0200

    Réintégration de Google Tag Manager dans le layout principal avec mise à jour de la méthode d'insertion pour améliorer la performance.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,17 +24,6 @@ export default function RootLayout({
   return (
     <html lang="fr" className="scroll-smooth">
       <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T5RDV2HW');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -51,6 +40,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <Script async src="https://tally.so/widgets/embed.js" />
       </head>
       <body className="bg-black font-sans text-white">
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T5RDV2HW');`}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
